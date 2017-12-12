380770133139615e50f27b9ac18f62db5626fef9
lalyos <lalyos@yahoo.com>
2017-12-12 20:49:05 +0100

    add AWS_ACCESS_KEY_ID, WS_SECRET_ACCESS_KEY, AWS_DEFAULT_REGION to graphQL variables

--- examples/cli/index.js
+++ examples/cli/index.js
@@ -5,8 +5,17 @@ import schema from './schema';
 import { graphql } from 'graphql';
 import getStdin from 'get-stdin';
 
- var variables =  JSON.parse(process.env.GQ_VARIABLES || '{}')
+var variables =  JSON.parse(process.env.GQ_VARIABLES || '{}')
 
+if (process.env.AWS_ACCESS_KEY_ID) {
+    variables.AWS_ACCESS_KEY_ID = process.env.AWS_ACCESS_KEY_ID 
+}
+if (process.env.AWS_SECRET_ACCESS_KEY) {
+    variables.AWS_SECRET_ACCESS_KEY = process.env.AWS_SECRET_ACCESS_KEY 
+}
+if (process.env.AWS_DEFAULT_REGION) {
+    variables.AWS_DEFAULT_REGION = process.env.AWS_DEFAULT_REGION 
+}
 getStdin().then(str => {
     //console.log(str);
     graphql(schema, str, null, null, variables).then((response) => {
